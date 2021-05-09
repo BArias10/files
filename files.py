a93df5b089fe17bd956d9ae32d6dbc343b0d09c4
BArias10 <83421510+BArias10@users.noreply.github.com>
2021-05-09 16:19:16 -0500

    main function

--- files.py
+++ files.py
@@ -1,1 +1,8 @@
 #Files Assignment Week 8
+
+def main():
+    directory = input("Enter Directory You Want To Save: ")
+    file_name = input("Enter Filename: ")
+    name = input("Enter Your Name: ")
+    phone_number = input("Enter Your Phone Number: ")
+    address = input("Enter Your Address: ")
