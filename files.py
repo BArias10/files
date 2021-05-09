c4c654008cd53fc7532b05cd4a88fbe7ae36e173
BArias10 <83421510+BArias10@users.noreply.github.com>
2021-05-09 16:27:08 -0500

    update 1

--- files.py
+++ files.py
@@ -6,3 +6,16 @@ def main():
     name = input("Enter Your Name: ")
     phone_number = input("Enter Your Phone Number: ")
     address = input("Enter Your Address: ")
+    if os.path.isdir(directory):
+        writeFile = open(os.path.join(directory, file_name), 'w')
+        writeFile.write(name + ',' + address + ',' + phone_number + '\n')
+        writeFile.close()
+
+        print("File Contents:")
+        readFile = open(os.path.join(directory, file_name), 'r')
+        for line in readFile:
+            print(line)
+        readFile.close()
+    else:
+        print("Directory Does Not Exist!")
+main()
